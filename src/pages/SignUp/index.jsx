14bb977fbe15c2ee41b6c710bd7af66acdb1604a
Cesar Augusto <gutoamorim@outlook.com>
2024-07-08 14:39:12 -0300

    Página de cadastro

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,3 +1,44 @@
+// import "./signin.css";
+import logo from "../../assets/logo.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 export const SignUp = () => {
-  return <div>Página de cadastro</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistyema de chamados" />
+        </div>
+        <form>
+          <h1>Nova Conta</h1>
+          <input
+            type="text"
+            placeholder="Seu Nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/">Ja possui uma conta? Faça login.</Link>
+      </div>
+    </div>
+  );
 };
